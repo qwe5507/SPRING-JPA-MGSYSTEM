ba04a9d5939e1c22b1f38334dedfded8b5aeb616
qwe5507 <apvmffkdls@naver.com>
2021-07-28 23:10:32 +0900

    02.HelloWordController 생성

---
+++ src/main/java/com/study/jpastudy/controller/HelloWordController.java
@@ -0,0 +1,14 @@
+package com.study.jpastudy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWordController {
+    @GetMapping(value = "/api/helloworld")
+    public String helloWorld(){
+        return "Hello World";
+    }
+}
